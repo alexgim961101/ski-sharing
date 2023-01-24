cce53d4c07706f0b34bb470e594e396ca026350c
alexgim <alexgim@naver.com>
2023-01-24 23:14:35 +0900

    swagger setting

---
+++ src/main/java/com/alexgim/sharing/config/SwaggerConfiguration.java
@@ -0,0 +1,56 @@
+package com.alexgim.sharing.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+public class SwaggerConfiguration {
+    private static final String API_NAME = "Spring Boot Application";
+    private static final String API_VERSION = "1.0.0";
+    private static final String API_DESCRIPTION = "스웨거 연습";
+
+    // 접속 경로 : http://localhost:8080/swagger-ui/
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo1"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+}
